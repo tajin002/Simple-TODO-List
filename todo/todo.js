064626fccf562d44131be794a2fe917f739357ed
tajin002 <tajinshawon002@gmail.com>
2022-12-07 18:55:42 +0600

    todo list funtionality done

--- todo/todo.js
+++ todo/todo.js
@@ -9,10 +9,24 @@ function addItem() {
     <th class = 'text-white' scope="row">${count}</th>
     <td class = 'text-white'>${inputText}</td>
     <td class = 'text-white'>
-        <button class="text-white bg-success border border-success p-1 rounded">Done</button>
-        <button class="text-white bg-danger border border-danger p-1 rounded">Delete</button>
+        <button class="done text-white bg-success border border-success p-1 rounded">Done</button>
+        <button class="delete text-white bg-danger border border-danger p-1 rounded">Delete</button>
     </td>
-
     `;
   contentContainer.appendChild(tableContainer);
-}
+
+  const doneItem = document.getElementsByClassName('done')
+  for(const button of doneItem){
+    button.addEventListener('click', function(){
+        event.target.parentNode.parentNode.style.textDecoration = 'line-through'
+    })
+  const deleteItem = document.getElementsByClassName('delete')
+  for(const button of deleteItem){
+    button.addEventListener('click', function(){
+        event.target.parentNode.parentNode.style.display = 'none'
+    })
+
+    
+  }
+
+}}
